c4dc57ccdeab919b1b1c47a156ac55eb5fb1c67e
prxm-b <uhry.julien@gmail.com>
2026-06-02 23:27:30 +0200

    Added Schematic of PCB in PDF

--- PIC10F200Demo.X/Toothbrush.asm
+++ PIC10F200Demo.X/Toothbrush.asm
@@ -5,107 +5,67 @@ PROCESSOR 10F200
 config WDTE = OFF, CP = OFF, MCLRE = OFF
 
 ; --- VARIABLES RAM ---
-PSECT udata,class=RAM,space=1,abs
-org 0x10
-SEC_COUNT:   DS 1
-SEC_TICK:    DS 1
-INNER_LOOP:  DS 1
-TEMP:        DS 1
+BRIGHTNESS  EQU 0x10    ; Niveau de luminosité actuel (0 à 255)
+COUNTER     EQU 0x11    ; Compteur pour la période PWM
+DELAY_VAL   EQU 0x12    ; Délai pour la vitesse de l'effet
+DIRECTION   EQU 0x13    ; 0 = augmente, 1 = diminue
 
-; --- CODE ---
 PSECT code,class=CODE,delta=2,abs
 org 0x00
 
 start:
-    MOVWF   OSCCAL
-    MOVLW   0       
+    MOVWF   OSCCAL      ; Calibration d'usine
+    MOVLW   0           ; Mode Digital I/O
     OPTION
-    MOVLW   0xFE    ; GP0 out, GP3 in
+    MOVLW   0xFE        ; GP0 en sortie
     TRIS    GPIO
-    BCF     GPIO, 0 
-
-wait_press:
-    ; 1. ATTENTE DE L'APPUI (GP3 passe à 1)
-    BTFSS   GPIO, 3 
-    GOTO    wait_press
     
-    ; 2. SÉCURITÉ : ATTENTE DU RELÂCHEMENT
-    ; On ne démarre que quand tu lâches le bouton
-    CALL    debounce
-wait_release_start:
-    BTFSC   GPIO, 3 
-    GOTO    wait_release_start
-    CALL    debounce
-    
-    ; --- INITIALISATION DU TIMER ---
-    MOVLW   120     ; 2 minutes
-    MOVWF   SEC_COUNT
-    BSF     GPIO, 0 ; Allumage LED
+    CLRF    BRIGHTNESS
+    CLRF    DIRECTION
 
-timer_loop:
-    MOVLW   10      
-    MOVWF   SEC_TICK
-
-second_block:
-    ; 3. TEST DU RÉ-APPUI POUR ÉTEINDRE
-    BTFSC   GPIO, 3
-    GOTO    force_stop
-    
-    CALL    delay_100ms
-    
-    DECFSZ  SEC_TICK, f
-    GOTO    second_block
-
-    ; --- TEST DU GLITCH (60 SEC) ---
-    MOVLW   60
-    XORWF   SEC_COUNT, W
-    BTFSS   STATUS, 2 
-    GOTO    check_end
-    
-    BCF     GPIO, 0
-    CALL    delay_100ms
-    BSF     GPIO, 0
-    CALL    delay_100ms
-    BCF     GPIO, 0
-    CALL    delay_100ms
-    BSF     GPIO, 0
-
-check_end:
-    DECFSZ  SEC_COUNT, f
-    GOTO    timer_loop
-
-force_stop:
-    BCF     GPIO, 0
-    CALL    debounce
-    
-    ; Attendre que le bouton soit relâché avant de pouvoir relancer
-wait_final_release:
-    BTFSC   GPIO, 3
-    GOTO    wait_final_release
-    CALL    debounce
-    GOTO    wait_press
-
-; --- SOUS-ROUTINES ---
-
-delay_100ms:
-    MOVLW   200
-    MOVWF   INNER_LOOP
-d100_1:
-    MOVLW   165
-    MOVWF   TEMP
-d100_2:
-    DECFSZ  TEMP, f
-    GOTO    d100_2
-    DECFSZ  INNER_LOOP, f
-    GOTO    d100_1
-    RETLW   0
-
-debounce:
+main_loop:
+    ; --- GÉNÉRATION PWM (1 cycle complet) ---
+    ; On compare COUNTER à BRIGHTNESS pour décider d'allumer ou éteindre
     MOVLW   255
-    MOVWF   TEMP
-db_1:
-    DECFSZ  TEMP, f
-    GOTO    db_1
-    RETLW   0
+    MOVWF   COUNTER
+    
+pwm_loop:
+    MOVF    BRIGHTNESS, W
+    SUBWF   COUNTER, W  ; W = COUNTER - BRIGHTNESS
+    
+    BTFSC   STATUS, 0   ; Test du Carry bit (C=1 si COUNTER >= BRIGHTNESS)
+    BCF     GPIO, 0     ; Éteindre LED
+    BTFSS   STATUS, 0   ; Test du Carry bit (C=0 si COUNTER < BRIGHTNESS)
+    BSF     GPIO, 0     ; Allumer LED
+    
+    DECFSZ  COUNTER, f
+    GOTO    pwm_loop
+
+    ; --- VITESSE DE L'EFFET ---
+    ; On ne change la luminosité que tous les X cycles PWM
+    DECFSZ  DELAY_VAL, f
+    GOTO    main_loop
+    MOVLW   1           ; Ajustez cette valeur pour changer la vitesse
+    MOVWF   DELAY_VAL
+
+    ; --- LOGIQUE DE GRADATION (FADE) ---
+    BTFSC   DIRECTION, 0
+    GOTO    decrease
+    
+increase:
+    INCF    BRIGHTNESS, f
+    MOVLW   254
+    XORWF   BRIGHTNESS, W
+    BTFSC   STATUS, 2   ; Si BRIGHTNESS == 254
+    BSF     DIRECTION, 0 ; On change de direction
+    GOTO    main_loop
+
+decrease:
+    DECF    BRIGHTNESS, f
+    MOVLW   1
+    XORWF   BRIGHTNESS, W
+    BTFSC   STATUS, 2   ; Si BRIGHTNESS == 1
+    BCF     DIRECTION, 0 ; On repasse en mode augmentation
+    GOTO    main_loop
 
 END start
